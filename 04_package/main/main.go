9d9b87452c54fda3df3dcee7ffdc67c276c67ef7
JokiNS <jokiminimal@gmail.com>
2017-05-14 14:22:07 +0200

    i remember this :)

---
+++ 04_package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "github.com/JokiNS/First-time/04_package/stringutil"
+)
+
+func main() {
+  fmt.Println(stringutil.Reverse("!oG ,olleH"))
+  fmt.Println(stringutil.MyName)
+}
