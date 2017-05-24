08d1e0bfe15dffa6753c22ffeb7a15a1ab6256af
JokiNS <jokiminimal@gmail.com>
2017-05-24 18:46:27 +0200

    this is easy, but need work to remember

---
+++ 13_switch-statements/02_fallthrough/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+  switch "Marcus" {
+  case "Jenny":
+    fmt.Println("Wassup Jenny")
+  case "Marcus":
+    fmt.Println("Wassup Marcus")
+    fallthrough
+  case "Medhi":
+    fmt.Println("Wassup Medhi")
+    fallthrough
+  case "Julian":
+    fmt.Println("Wassup Julian")
+  case "Sushant":
+    fmt.Println("Wassup Sushant")
+  }
+}
